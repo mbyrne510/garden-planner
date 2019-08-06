9597ee6dbf370c04899956c9f6af12a0ccb1d791
mbyrne510 <byrnemat@oregonstate.edu>
2019-08-05 20:52:15 -0700

    Add initial get/post/put requests in GardenPlanner

--- src/components/Garden/Garden.js
+++ src/components/Garden/Garden.js
@@ -4,19 +4,24 @@ import Grid from '@material-ui/core/Grid';
 //import GardenControls from './GardenControls';
 
 const garden = (props) => {
-    const beds = [...Array(props.beds)].map((bed, i) => {
-        return <Bed key={i}/>
-    });
-    return (
-        <React.Fragment>
-            <Grid 
-                container
-                spacing={2}
-                justify="center">
-                {beds}
-            </Grid>
-        </React.Fragment>
-    )
+    if (props.bedCt > 0) {
+        const beds = Object.keys(props.beds).map((bed, i) => {
+            return <Bed key={i}/>
+        });
+        return (
+            <React.Fragment>
+                <Grid 
+                    container
+                    spacing={2}
+                    justify="center">
+                    {beds}
+                </Grid>
+            </React.Fragment>
+        );
+    }
+    else {
+        return null;
+    }
 }
 
 export default garden;
--- src/containers/GardenPlanner/GardenPlanner.js
+++ src/containers/GardenPlanner/GardenPlanner.js
@@ -5,65 +5,60 @@ import axios from '../../axios-garden';
 
 class GardenPlanner extends Component {
     state = {
-        beds: 0,
-        bedLayouts: {
-            plants: {
-                artichoke: 0,
-                blueberry: 0,
-                broccoli: 0,
-                carrots: 0,
-                lettuce: 0,
-                onions: 0,
-                peas: 0,
-                potatoes: 0,
-                strawberries: 0,
-                tomatoes: 0,
-                watermelon: 0        
-            }
-        }
+        bedCt: 0,
+        bedLayouts: null,
     }
 
     componentDidMount() {
-        axios.get('/beds.json')
+        axios.get('/.json')
             .then(response => {
-                this.setState({bedLayouts: response.data});
-                console.log(response.data);
+                this.setState({bedCt: response.data.bedCt, bedLayouts: response.data.beds});
             });
     }
 
     addBedHandler = () => {
-        let bedCt = this.state.beds;
-        bedCt++;
-        this.setState({beds: bedCt});
-        const newBed = {
+        let newBedCt = this.state.bedCt;
+        newBedCt++;
+        this.setState({bedCt: newBedCt});
+        const newBed = Array.from(Array(4), () => new Array(4));
+        const update = {
             plants: {
-                artichoke: 0,
-                blueberry: 0,
-                broccoli: 0,
-                carrots: 0,
-                lettuce: 0,
-                onions: 0,
-                peas: 0,
-                potatoes: 0,
-                strawberries: 0,
-                tomatoes: 0,
-                watermelon: 0        
+                r1c1: 0,
+                r1c2: 0,
+                r1c3: 0,
+                r1c4: 0,
+                r2c1: 0,
+                r2c2: 0,
+                r2c3: 0,
+                r2c4: 0,
+                r3c1: 0,
+                r3c2: 0,
+                r3c3: 0,
+                r3c4: 0,
+                r4c1: 0,
+                r4c2: 0,
+                r4c3: 0,
+                r4c4: 0
             }
         };
-        axios.post('/beds.json', newBed)
+        console.log(update);
+        axios.post('/beds.json', update)
             .then(response => {
-                // console.log('data posted');
+                console.log('received');
             });
-        axios.put('/bedCt.json', bedCt)
+        axios.put('/bedCt.json', newBedCt)
             .then(response => {
-                // console.log('data updated');
+                axios.get('/.json')
+                .then(response => {
+                    this.setState({bedCt: response.data.bedCt, bedLayouts: response.data.beds});
+                });        
             });
     }
 
     remBedHandler = () => {
-        let bedCt = this.state.beds;
-        bedCt--;
-        this.setState({beds: bedCt});
+        let newBedCt = this.state.bedCt;
+        newBedCt--;
+        this.setState({beds: newBedCt});
     }
 
     render() {
@@ -72,7 +67,7 @@ class GardenPlanner extends Component {
                 <GardenControls 
                     added={this.addBedHandler}
                     removed={this.remBedHandler} />
-                <Garden beds={this.state.beds} />
+                <Garden beds={this.state.bedLayouts} bedCt={this.state.bedCt} />
             </div>
         )
     }
